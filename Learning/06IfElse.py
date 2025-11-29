0739f2cc5881f66cba3a1c05dd1dc854f9d38b9f
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-11-29 22:04:50 +0530

    If Else

---
+++ Learning/06IfElse.py
@@ -0,0 +1,21 @@
+a = int(input("enter age: "))
+print("your age is: ", a)
+if(a>18):
+    print("you can drive")
+else:
+    print("you cannot drive")
+
+
+print(a>18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+print()
+applePrice = 10
+budget = 200
+if(budget - applePrice > 50) :
+    print("Add 1kg apples")
+elif(budget - applePrice > 70) :
+    print("Its okay you can buy")
+else :
+    print("Dont buy")
