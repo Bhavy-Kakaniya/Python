b0f3809bff825c0cdb8534d0c7ec1a87a65e1b31
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-11-30 22:39:40 +0530

    for loops and while loop

---
+++ Learning/08ForWhileLoop.py
@@ -0,0 +1,14 @@
+name = 'bhavy'
+for i in name:
+    print(i)
+
+colors = ["red", "green", "blue"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+i = 0
+while(i < 4):
+    print(i)
+    i += 1
