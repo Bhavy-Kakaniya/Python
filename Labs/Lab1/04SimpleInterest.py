4d7bc460f992f3b03e2c6ebc3a226a6a05811072
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-12-01 21:12:49 +0530

    simple interest calculation

---
+++ Labs/Lab1/04SimpleInterest.py
@@ -0,0 +1,6 @@
+p = int(input("Enter principle: "))
+r = int(input("Enter Rate: "))
+t = int(input("Enter Time: "))
+simpleInterest = (p * r * t) / 100
+print("Simple interest is ", simpleInterest)
+print("Net amount is ", simpleInterest + p)
