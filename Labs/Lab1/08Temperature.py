b4aa529dd51cb8b85cae740b80b6676f6c44f3d2
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-12-02 21:53:07 +0530

    temperature conversion between celsius and fahrenheit

---
+++ Labs/Lab1/08Temperature.py
@@ -0,0 +1,6 @@
+c = float(input("Enter celsius: "))
+f = (c * 9 / 5) + 32
+print(c, "degree Celsius in fahrenheit: ", f)
+f = float(input("Enter fahrenheit: "))
+c = (f - 32) * 5 / 9
+print(f, "fahrenheit in degree celsius", c)
