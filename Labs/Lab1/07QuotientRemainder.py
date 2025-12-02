c234f64eb7573bd21066e1bf2770ff74eb760277
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-12-02 21:52:29 +0530

    quotient and remainder calculation

---
+++ Labs/Lab1/07QuotientRemainder.py
@@ -0,0 +1,8 @@
+a = int(input("Enter dividend: "))
+b = int(input("Enter divisor"))
+quotient = a // b
+remainder = a % b
+print("Dividend = ", a)
+print("Divisor = ", b)
+print("Quotient = ", quotient)
+print("Remainder = ", remainder)
