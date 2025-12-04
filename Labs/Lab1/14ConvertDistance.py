6b928fe14e2104e9c3f1560891c3b3a4da029b2c
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-12-04 22:04:55 +0530

    distance conversion from kilometers to various units

---
+++ Labs/Lab1/14ConvertDistance.py
@@ -0,0 +1,6 @@
+km = int(input("Enter distance in kilometer"))
+m = km * 1000
+cm = m * 1000
+inch = cm / 2.54
+feet = inch / 12
+print(km, "km =", m, "m =", cm, "cm =", inch, "inches =", feet, "feets")
