b618dbaafb86c32eb47400edb33724da209a7c80
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-12-14 20:12:35 +0530

    palindrome number

---
+++ Labs/Lab3/13Palindrome.py
@@ -0,0 +1,8 @@
+# check whether the given number is palindrome or not
+n = int(input("Enter a number: "))
+temp = n
+rev = 0
+while(temp > 0):
+    rev = (rev * 10) + (temp % 10)
+    temp = temp//10
+print(n, "is palindrome") if(rev == n) else print(n, "is not palindrome")
